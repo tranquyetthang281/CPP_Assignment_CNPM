1f70e6786d872926ea988c0a3093d99da76ced5b
Thang Tran Quyet <tranquyetthang281@gmail.com>
2021-11-21 16:05:18 +0700

    Fix DetailView

--- SourceMVC/client/mvc/views/content/DetailItemView.php
+++ SourceMVC/client/mvc/views/content/DetailItemView.php
@@ -3,7 +3,7 @@
         <div class="modal-dialog  modal-dialog-centered modal-xl" role="document">
             <div class="modal-content proper">
                 <div class="modal-header d-flex justify-content-center">
-                    <h5 class="modal-title " id="title">BURGER BÒ NƯỚNG WHOPPER</h5>
+                    <h5 class="modal-title " id="title"><?php echo $item['name'] ?></h5>
                     <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button> -->
@@ -11,46 +11,19 @@
                 <div class="modal-body">
                     <div class="container-fluid">
                         <div class="row">
-                            <div class="col-md-6"><img src="./img/burger.png" alt="  " /></div>
+                            <div class="col-md-6"><img class="img-detail"src="http://localhost/CPP_Assignment_CNPM/SourceMVC/images/<?php echo $item['image'] ?>" alt="  " /></div>
                             <div class="col-md-6">
-                                <div class="disc">
-                                    <p>Burger bò nướng whopper</p>
+                                <div class="disc-detail">
+                                    <p><?php echo $item['name'] ?></p>
                                 </div>
-                                <div class="calories">
+                                <div class="calories-detail">
                                     <div class="row  row1 ">
-                                        <p style="margin: 0 auto">520 Calories</p>
+                                        <p style="margin: 0 auto"><?php echo $item['calories'] ?> Calories</p>
                                     </div>
                                     <div class="row row2">
-                                        <div class="col-md-3">
+                                        <div class="col-md-12">
                                             <div class="row">
-                                                <p>Fat</p>
-                                            </div>
-                                            <div class="row">
-                                                <p>14g</p>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-3">
-                                            <div class="row">
-                                                <p>Carbs</p>
-                                            </div>
-                                            <div class="row">
-                                                <p>85g</p>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-3">
-                                            <div class="row">
-                                                <p>Fiber</p>
-                                            </div>
-                                            <div class="row">
-                                                <p>1g</p>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-3">
-                                            <div class="row">
-                                                <p>Protein</p>
-                                            </div>
-                                            <div class="row">
-                                                <p>12g</p>
+                                                <p><?php echo $item['description'] ?></p>
                                             </div>
                                         </div>
                                     </div>
@@ -58,19 +31,19 @@
                                 </div>
                                 <div class="row" style="margin-bottom: 20px;">
                                     <div class="col-md-9">
-                                        <label for="buttons_added"><b>Số lượng:</b></label><br>
-                                        <div class="buttons_added">
-                                            <input class="minus is-form" type="button" value="-" onclick=" decPrice()">
-                                            <input aria-label="quantity" class="input-qty" max="10" min="0" name="soluong" type="number" value="0">
-                                            <input class="plus is-form" type="button" value="+" onclick=" incPrice()">
+                                        <label for="buttons-added-detail"><b>Số lượng:</b></label><br>
+                                        <div class="buttons-added-detail">
+                                            <input class="minus is-form-detail" type="button" value="-" onclick=" decPrice()">
+                                            <input aria-label="quantity" class="input-qty-detail" max="10" min="0" name="soluong" type="number" value="0">
+                                            <input class="plus is-form-detail" type="button" value="+" onclick=" incPrice()">
                                         </div>
                                     </div>
                                     <div class="col-md-3 d-flex justify-content-center align-items-center">
-                                        <span id="price"> 0đ </span>
+                                        <span id="price-detail"> 0đ </span>
                                     </div>
                                 </div>
                                 <div class="row">
-                                    <button type="button" class="btn btn-danger">Thêm vào giỏ hàng</button>
+                                    <button type="button" class="btn btn-danger">Buy Now</button>
                                 </div>
                             </div>
                         </div>
@@ -79,32 +52,3 @@
             </div>
         </div>
     </div>
-
-
-
-    <script>
-        $('input.input-qty').each(function() {
-            var $this = $(this),
-                qty = $this.parent().find('.is-form'),
-                min = Number($this.attr('min')),
-                max = Number($this.attr('max'))
-            if (min == 0) {
-                var d = 0
-            } else d = min
-            $(qty).on('click', function() {
-                if ($(this).hasClass('minus')) {
-                    if (d > min) {
-                        d += -1;
-                        document.getElementById("price").innerText = Number(d) * 200 + ".000đ";
-                    }
-                } else if ($(this).hasClass('plus')) {
-                    var x = Number($this.val()) + 1
-                    if (x <= max) {
-                        d += 1;
-                        document.getElementById("price").innerText = Number(d) * 200 + ".000đ";
-                    }
-                }
-                $this.attr('value', d).val(d)
-            })
-        })
-    </script>
--- SourceMVC/client/public/js/script.js
+++ SourceMVC/client/public/js/script.js
@@ -91,3 +91,29 @@ $(".search-item").keyup(function () {
         },
     });
 });
+
+//handle plus num detail
+$('input.input-qty-detail').each(function() {
+    var $this = $(this),
+        qty = $this.parent().find('.is-form-detail'),
+        min = Number($this.attr('min')),
+        max = Number($this.attr('max'))
+    if (min == 0) {
+        var d = 0
+    } else d = min
+    $(qty).on('click', function() {
+        if ($(this).hasClass('minus')) {
+            if (d > min) {
+                d += -1;
+                document.getElementById("price-detail").innerText = Number(d) * 200 + ".000đ";
+            }
+        } else if ($(this).hasClass('plus')) {
+            var x = Number($this.val()) + 1
+            if (x <= max) {
+                d += 1;
+                document.getElementById("price-detail").innerText = Number(d) * 200 + ".000đ";
+            }
+        }
+        $this.attr('value', d).val(d)
+    })
+})
