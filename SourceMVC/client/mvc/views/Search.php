27b950a3bb9c15f90ee8d948f73860ed8b7d2b74
Thang Tran Quyet <tranquyetthang281@gmail.com>
2021-11-21 16:13:39 +0700

    Add Search Detail

--- SourceMVC/client/mvc/views/Search.php
+++ SourceMVC/client/mvc/views/Search.php
@@ -1,15 +1,16 @@
 <?php if ($data) {
-    foreach ($data as $key => $value) {
+    foreach ($data as $key => $item) {
 ?>
-        <div class="item-search-1 item-search-box mt-3 mb-2">
+        <div class="item-search-1 item-search-box mt-3 mb-2" type="button" data-bs-toggle="modal" data-bs-target="#detail">
             <div class="image">
-                <img src="http://localhost/CPP_Assignment_CNPM/SourceMVC/images/<?php echo $value['image'] ?>" alt="">
+                <img src="http://localhost/CPP_Assignment_CNPM/SourceMVC/images/<?php echo $item['image'] ?>" alt="">
             </div>
             <div class="name">
-                <?php echo $value['name'] ?>
+                <?php echo $item['name'] ?>
             </div>
-        </div>
-
+        </div>  
+         
+        <?php require_once "content/DetailItemView.php" ?>
 <?php
     }
 } ?>
