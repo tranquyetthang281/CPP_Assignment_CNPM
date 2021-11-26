b79d13e5ce60182381f2ecbb2da1a9d43f839084
knailk <tiendung3719@gmail.com>
2021-11-26 15:49:06 +0700

    updata

--- SourceMVC/client/mvc/views/Home.php
+++ SourceMVC/client/mvc/views/Home.php
@@ -26,7 +26,7 @@ $DOMAIN = 'http://localhost/CPP_Assignment_CNPM/SourceMVC/client';
     <!-- header start -->
     <header>
         <div class="title">
-        <a href="<?php echo $DOMAIN ?>" style="text-decoration: none;">
+            <a href="<?php echo $DOMAIN ?>" style="text-decoration: none;">
                 <img src="http://localhost/CPP_Assignment_CNPM/SourceMVC/images/logo.png" height="100px">
             </a>
         </div>
@@ -34,14 +34,17 @@ $DOMAIN = 'http://localhost/CPP_Assignment_CNPM/SourceMVC/client';
             <input type="text" class="form-control search-item" placeholder="Search..." />
             <ion-icon class="icon-search" name="search-outline"></ion-icon>
         </div>
-        <!-- <div>About us</div> -->
-        <div class="contact">
-            <a href="<?php echo $DOMAIN ?>/Contact/Show"> Contact us </a>
+        <div id="ac-us">
+            <ul id="ac-us1">
+            <li> <a href="<?php echo $DOMAIN ?>/About/Show"> About us </a></li>
+            <li> <a href="<?php echo $DOMAIN ?>/Contact/Show"> Contact us </a></li>
+            </ul>
         </div>
+        
         <div class="user-action">
             <?php if (isset($_SESSION['token_user'])) { ?>
-                <div class="dropdown">
-                    <ion-icon name="person-circle-outline" class="icon-profile" data-bs-toggle="dropdown"></ion-icon>
+                <div class="dropdown" style="margin-top: -30px">
+                    <i class="fa fa-user text-warning icon-profile" data-bs-toggle="dropdown" aria-hidden="true" ></i>
                     <ul class="dropdown-menu drop-menu-end">
                         <li><a class="dropdown-item" href="<?php echo $DOMAIN ?>/Login/accountPage">Edit Profile</a></li>
                         <li><a class="dropdown-item" href="<?php echo $DOMAIN ?>/Order/historyPage">Payment History</a></li>
@@ -49,7 +52,7 @@ $DOMAIN = 'http://localhost/CPP_Assignment_CNPM/SourceMVC/client';
                     </ul>
                 </div>
             <?php } else { ?>
-                <a href="<?php echo $DOMAIN ?>/Login/loginPage"  class="lg-icon"><i class="fa fa-sign-in text-warning" aria-hidden="true"></i></a>
+                <a href="<?php echo $DOMAIN ?>/Login/loginPage" class="lg-icon"><i class="fa fa-sign-in text-warning" aria-hidden="true"></i></a>
             <?php } ?>
         </div>
         <?php if ($data['page'] != 'Checkout') { ?>
@@ -92,74 +95,76 @@ $DOMAIN = 'http://localhost/CPP_Assignment_CNPM/SourceMVC/client';
     </div>
     <!-- footer start -->
     <div class="footer">
-                <div class="footer-static">
-                    <div class="container-955" >
-                        <div class="footer-logo-wrap pt-50 pb-35" >
-                            <div class="row">
-                                <!-- Block 1 -->
-                                <div class="col-lg-4" >
-                                    <div class="footer-block">
-                                        <h3 class="footer-block-title">About Our Restaurant</h3>
-                                        <p style="max-width:250px;">Breakfast buffet: Everyday | 6am – 10am (10:30am on Weekend)</p>
-                                        <p style="max-width:250px;">Chic Buffet Lunch: Wednesday – Sunday | 12pm – 2pm</p>
-                                        <ul class="social-link">
-                                            <li class="list-inline-item">
-                                                <a href="#" id="twitter">
-                                                    <ion-icon name="logo-twitter" size="large"></ion-icon>
-                                                </a>
-                                            </li>
-                                            <li class="list-inline-item">
-                                                <a href="#" id="facebook">
-                                                    <ion-icon name="logo-facebook" size="large"></ion-icon>
-                                                </a>
-                                            </li>
-                                            <li  class="list-inline-item">
-                                                <a href="#" id="instagram">
-                                                    <ion-icon name="logo-instagram" size="large"></ion-icon>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                            </div>  
-                                <!--Block 2 -->
-                                <div class="col-lg-4 col-md-4 col-sm">
-                                    <div class="footer-block">
-                                        <h3 class="footer-block-title">Have a Questions?</h3>
-                                        <ul>
-                                            <div>
-                                                <i class="fa fa-map-marker"></i>
-                                                <p><span>444 S. Cedros Ave</span> Solana Beach, California</p>
-                                              </div>
-                                              <div>
-                                                <i class="fa fa-phone"></i>
-                                                <p>+1.555.555.5555</p>
-                                              </div>
-                                      
-                                              <div>
-                                                <i class="fa fa-envelope"></i>
-                                                <p><a href="#">support@company.com</a></p>
-                                              </div>
-                                            
-                                        </ul>
-                                    </div>
-                                </div>
-                                <!--Block 3 -->
-                                <div class="col-lg-4 col-md-4 col-sm">
-                                    <div class="footer-block">
-                                    <h3 class="footer-block-title">Thưởng thức theo cách của bạn</h3>
-                                        <ul>
-                                        <li><a href="<?php echo $DOMAIN ?>" style="text-decoration: none;">
-                                    <img src="http://localhost/Project/LapTrinhWeb/assignment/SourceMVC/images/logo.png" height="150px" style="margin-left: 100px;"></a></li>
-                                        </ul>
-                                    </div>
-                                </div>
+        <div class="footer-static">
+            <div class="container-955">
+                <div class="footer-logo-wrap pt-50 pb-35">
+                    <div class="row">
+                        <!-- Block 1 -->
+                        <div class="col-lg-4">
+                            <div class="footer-block">
+                                <h3 class="footer-block-title">About Our Restaurant</h3>
+                                <p style="max-width:250px;">Breakfast buffet: Everyday | 6am – 10am (10:30am on Weekend)</p>
+                                <p style="max-width:250px;">Chic Buffet Lunch: Wednesday – Sunday | 12pm – 2pm</p>
+                                <ul class="social-link">
+                                    <li class="list-inline-item">
+                                        <a href="#" id="twitter">
+                                            <ion-icon name="logo-twitter" size="large"></ion-icon>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a href="#" id="facebook">
+                                            <ion-icon name="logo-facebook" size="large"></ion-icon>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a href="#" id="instagram">
+                                            <ion-icon name="logo-instagram" size="large"></ion-icon>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        
+                        <!--Block 2 -->
+                        <div class="col-lg-4 col-md-4 col-sm">
+                            <div class="footer-block">
+                                <h3 class="footer-block-title">Have a Questions?</h3>
+                                <ul>
+                                    <div>
+                                        <i class="fa fa-map-marker"></i>
+                                        <p><span>444 S. Cedros Ave</span> Solana Beach, California</p>
+                                    </div>
+                                    <div>
+                                        <i class="fa fa-phone"></i>
+                                        <p>+1.555.555.5555</p>
+                                    </div>
+
+                                    <div>
+                                        <i class="fa fa-envelope"></i>
+                                        <p><a href="#">support@company.com</a></p>
+                                    </div>
+
+                                </ul>
+                            </div>
+                        </div>
+                        <!--Block 3 -->
+                        <div class="col-lg-4 col-md-4 col-sm">
+                            <div class="footer-block">
+                                <h3 class="footer-block-title">Thưởng thức theo cách của bạn</h3>
+                                <ul>
+                                    <li><a href="<?php echo $DOMAIN ?>" style="text-decoration: none;">
+                                            <img src="http://localhost/Project/LapTrinhWeb/assignment/SourceMVC/images/logo.png" height="150px" style="margin-left: 100px;"></a></li>
+
+                                   
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                    
+
                 </div>
+
             </div>
-            </div>
+        </div>
+    </div>
     <!-- footer end -->
     <script>
 
